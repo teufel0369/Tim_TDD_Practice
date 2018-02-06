8cb7b73b450192484d27411d827b6e7a55ea91f4
Chris Thompson <cjt7g6@umsl.edu>
2018-02-06 17:38:22 -0600

    Initial commit

---
+++ before/order-service/src/main/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformer.java
@@ -0,0 +1,13 @@
+package com.bigbox.b2csite.order.model.transformer;
+
+import java.math.BigDecimal;
+
+import com.bigbox.b2csite.order.model.domain.OrderSummary;
+import com.bigbox.b2csite.order.model.entity.OrderEntity;
+import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
+
+
+
+public class OrderEntityToOrderSummaryTransformer {
+
+}
---
+++ before/order-service/src/test/java/com/bigbox/b2csite/order/model/transformer/OrderEntityToOrderSummaryTransformerTest.java
@@ -0,0 +1,20 @@
+package com.bigbox.b2csite.order.model.transformer;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.bigbox.b2csite.order.model.domain.OrderSummary;
+import com.bigbox.b2csite.order.model.entity.OrderEntity;
+import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
+
+
+
+public class OrderEntityToOrderSummaryTransformerTest {
+
+}
